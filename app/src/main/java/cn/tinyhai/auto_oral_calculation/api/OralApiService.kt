980bde06bb69e890c832c876ec775c2c1e4eff90
TinyHai <1035908357@qq.com>
2024-11-09 14:32:27 +0800

    dont block CancellationException anymore

--- app/src/main/java/cn/tinyhai/auto_oral_calculation/api/OralApiService.kt
+++ app/src/main/java/cn/tinyhai/auto_oral_calculation/api/OralApiService.kt
@@ -4,74 +4,44 @@ import de.robv.android.xposed.XposedHelpers
 import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
 import java.lang.reflect.Proxy
-import kotlin.coroutines.cancellation.CancellationException
 
 object OralApiService {
 
-    private class UploadExamResultCoroutine(
+    private class ContinuationProxy(
         private val coroutineContext: Any,
-        private val onResult: (Result<Unit>) -> Unit
+        private val onResume: (Result<Any>) -> Unit
     ) : InvocationHandler {
         override fun invoke(proxy: Any?, method: Method, args: Array<out Any>?): Any? {
             when (method.name) {
                 "getContext" -> {
                     return coroutineContext
                 }
+
                 "resumeWith" -> {
                     val result = args?.get(0)
                     val ret = when {
                         result == null -> {
-                            Result.failure(NullPointerException("UploadExamResult result is null"))
+                            Result.failure(NullPointerException("result is null"))
                         }
-                        result::class.java.name == "kotlin.Result\$Failure" -> {
-                            Result.failure(XposedHelpers.getObjectField(result, "exception") as Throwable)
-                        }
-                        result::class.java.name == "kotlin.Result" -> {
-                            Result.success(Unit)
-                        }
-                        else -> {
-                            Result.success(Unit)
-                        }
-                    }
-                    if (ret.exceptionOrNull() !is CancellationException) {
-                        onResult(ret)
-                    }
 
-                    return null
-                }
-            }
-            return null
-        }
-    }
-
-    private class GetExamInfoCoroutine(
-        private val coroutineContext: Any,
-        private val onResult: (Result<Any>) -> Unit
-    ) : InvocationHandler {
-        override fun invoke(proxy: Any?, method: Method, args: Array<out Any>?): Any? {
-            when (method.name) {
-                "getContext" -> {
-                    return coroutineContext
-                }
-                "resumeWith" -> {
-                    val result = args?.get(0)
-                    val ret = when {
-                        result == null -> {
-                            Result.failure(NullPointerException("getExamInfo result is null"))
-                        }
                         result::class.java.name == "kotlin.Result\$Failure" -> {
-                            Result.failure(XposedHelpers.getObjectField(result, "exception") as Throwable)
+                            Result.failure(
+                                XposedHelpers.getObjectField(
+                                    result,
+                                    "exception"
+                                ) as Throwable
+                            )
                         }
+
                         result::class.java.name == "kotlin.Result" -> {
                             Result.success(XposedHelpers.getObjectField(result, "value"))
                         }
+
                         else -> {
                             Result.success(result)
                         }
                     }
-                    if (ret.exceptionOrNull() !is CancellationException) {
-                        onResult(ret)
-                    }
+                    onResume(ret)
 
                     return null
                 }
@@ -110,15 +80,19 @@ object OralApiService {
 
     fun getExamInfo(onResult: (Result<Any>) -> Unit) {
         val coroutineClass = getExamInfoMethod.parameterTypes[2]
-        val getExamInfoProxy = Proxy.newProxyInstance(coroutineClass.classLoader, arrayOf(coroutineClass), GetExamInfoCoroutine(
-            coroutineContext, onResult))
+        val getExamInfoProxy = Proxy.newProxyInstance(
+            coroutineClass.classLoader,
+            arrayOf(coroutineClass),
+            ContinuationProxy(coroutineContext, onResult)
+        )
         getExamInfoMethod.invoke(apiService, keyPointId, limit, getExamInfoProxy)
     }
 
-    fun uploadExamResult(examId: String, requestData: Any, onResult: (Result<Unit>) -> Unit) {
+    fun uploadExamResult(examId: String, requestData: Any, onResult: (Result<Any>) -> Unit) {
         val coroutineClass = uploadExamResultMethod.parameterTypes[2]
-        val uploadExamResultProxy = Proxy.newProxyInstance(coroutineClass.classLoader, arrayOf(coroutineClass), UploadExamResultCoroutine(
-            coroutineContext, onResult))
+        val uploadExamResultProxy = Proxy.newProxyInstance(
+            coroutineClass.classLoader, arrayOf(coroutineClass), ContinuationProxy(coroutineContext, onResult)
+        )
         uploadExamResultMethod.invoke(apiService, examId, requestData, uploadExamResultProxy)
     }
 }
